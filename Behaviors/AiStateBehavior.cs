057d89fc79a7531005b1fccb6827b8d62546d036
Anthony Romrell <anthonyromrell@mac.com>
2018-04-05 12:45:53 -0600

    Added AI componets

---
+++ Behaviors/AiStateBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiStateBehavior : MonoBehaviour {
+
+	private NavMeshAgent aiAgent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	private void OnTriggerEnter(Collider other) {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
