b9b1491d3b0deaf37cece5ba3de08672500e1400
Anthony Romrell <anthony@artisandream.com>
2018-05-19 17:49:23 -0600

    Updated PatrolPoints to be User Configured.

---
+++ Scripts/AddPatrolPointsList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Made By Anthony Romrell
+public class AddPatrolPointsList : MonoBehaviour
+{
+
+	public List<Transform> PatrolPoints;
+	
+	public GameAction SendAction;
+
+	private void Start()
+	{
+		SendAction.Call(PatrolPoints);
+	}
+
+}
